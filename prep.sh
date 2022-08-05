769e51a08f23c60fd90d03015707b840e662e505
Brian Xiao <bxiao23@mit.edu>
2022-08-05 11:48:27 -0400

    unified version of prep.sh

--- prep.sh
+++ prep.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-ALF_HOME="/home/bxiao23/alf/ALF-master"
-PYALF_HOME="/home/ganzfeld/fourelectron/alf/pyALF-master"
+case "$1" in
+    "r"|"roch")
+        ALF_HOME="/home/bxiao23/alf/ALF-master"
+        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
+        ;;
+    *)
+        ALF_HOME="/home/ganzfeld/fourelectron/alf/ALF-master"
+        PYALF_HOME="/home/ganzfeld/fourelectron/alf/pyALF-master"
+esac
 
 if [[ "${PYTHONPATH-}" != *"$PYALF_HOME"* ]]; then
     export PYTHONPATH="$PYALF_HOME:$PYTHONPATH"
