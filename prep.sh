9df599d468318ddc9cd3b1ca71af0f796654400c
Brian Xiao <bxiao23@mit.edu>
2022-08-22 14:35:01 -0400

    more copies of ALF-master! (kms)

--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 case "$1" in
-    "r"|"roch")
+    "r"|"roch"|"r1"|"roch1")
         ALF_HOME="/home/bxiao23/alf/ALF-master"
         PYALF_HOME="/home/bxiao23/alf/pyALF-master"
         ;;
@@ -9,9 +9,22 @@ case "$1" in
         ALF_HOME="/home/bxiao23/alf/ALF-master2"
         PYALF_HOME="/home/bxiao23/alf/pyALF-master"
         ;;
+    "r3"|"roch3")
+        ALF_HOME="/home/bxiao23/alf/ALF-master3"
+        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
+        ;;
+    "r4"|"roch4")
+        ALF_HOME="/home/bxiao23/alf/ALF-master4"
+        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
+        ;;
+    "r5"|"roch5")
+        ALF_HOME="/home/bxiao23/alf/ALF-master5"
+        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
+        ;;
     *)
         ALF_HOME="/home/ganzfeld/fourelectron/alf/ALF-master"
         PYALF_HOME="/home/ganzfeld/fourelectron/alf/pyALF-master"
+        ;;
 esac
 
 if [[ "${PYTHONPATH-}" != *"$PYALF_HOME"* ]]; then
