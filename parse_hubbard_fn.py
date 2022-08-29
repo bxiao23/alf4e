4724e6ff69326b12859dfabafe92efc0c38202df
Brian Xiao <bxiao23@mit.edu>
2022-08-29 17:31:59 -0400

    updates to filename/parameter translation

--- parse_hubbard_fn.py
+++ parse_hubbard_fn.py
@@ -1,10 +1,27 @@
-ALF_FMT_KEYS = ["ham_u", "ham_chem", "T", "Dtau", "beta"]
-KEYS = ["U", "mu", "t", "dtau", "beta"]
+# some tags have underscores in them -- very annoying
+# this still works for now but be careful when adding new things
+ALF_FMT_KEYS = ["L1", "L2",
+                "u", "chem", "T",
+                "Dtau", "beta",
+                "Projector", "Theta",
+                "X", "Y"]
+KEYS =         ["Lx", "Ly",
+                "U", "mu", "t",
+                "dtau", "beta",
+                "projector", "theta",
+                "phi_x", "phi_y"]
 
 def parse_fn(fn):
     entries = fn.split("_")
     entries = dict(e.split("=") for e in entries if "=" in e)
-    return {k:entries[alf_k] for alf_k, k in zip(ALF_FMT_KEYS, KEYS)}
+    return {k:entries.get(alf_k, None) for alf_k, k in zip(ALF_FMT_KEYS, KEYS)}
+
+def make_fn(name="Hubbard", params=None):
+    from py_alf import ALF_source, Simulation
+    if params is None:
+        params = {}
+    sim = Simulation(ALF_source(), name, params)
+    return sim.sim_dir
 
 if __name__ == "__main__":
     pass
