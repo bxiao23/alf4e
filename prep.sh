076ed094c98e6cb8da4f6b422ac76be37df38ccc
Brian Xiao <bxiao23@mit.edu>
2022-08-29 17:32:39 -0400

    generic handling of ALF-master dir on roch

--- prep.sh
+++ prep.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
 case "$1" in
-    "r"|"roch"|"r1"|"roch1")
-        ALF_HOME="/home/bxiao23/alf/ALF-master"
+    "r"*)
         PYALF_HOME="/home/bxiao23/alf/pyALF-master"
-        ;;
-    "r2"|"roch2")
-        ALF_HOME="/home/bxiao23/alf/ALF-master2"
-        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
-        ;;
-    "r3"|"roch3")
-        ALF_HOME="/home/bxiao23/alf/ALF-master3"
-        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
-        ;;
-    "r4"|"roch4")
-        ALF_HOME="/home/bxiao23/alf/ALF-master4"
-        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
-        ;;
-    "r5"|"roch5")
-        ALF_HOME="/home/bxiao23/alf/ALF-master5"
-        PYALF_HOME="/home/bxiao23/alf/pyALF-master"
+        tag=${1:1}
+        # for compatibility; might change someday
+        if [ "$tag" == "1" ]; then
+            ALF_HOME="/home/bxiao23/alf/ALF-master"
+        else
+            ALF_HOME="/home/bxiao23/alf/ALF-master${tag}"
+        fi
         ;;
     *)
         ALF_HOME="/home/ganzfeld/fourelectron/alf/ALF-master"
